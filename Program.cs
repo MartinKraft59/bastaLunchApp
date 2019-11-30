47f1d3a043ec03330cca93587bd574ef55230344
martin <martinkraft@t-online.de>
2019-11-30 11:08:00 +0100

    write Basta 2019

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace bastaLunchApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World 2019");
         }
     }
 }
