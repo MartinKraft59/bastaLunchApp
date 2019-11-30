fc240f8ad0da1352afdccb0ce61385087ba9943d
martin <martinkraft@t-online.de>
2019-11-30 09:41:47 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bastaLunchApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
